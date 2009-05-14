4fbd3e2438c30b024f92e0cd9679aa6875c270fd
Farzad FARID <ffarid@pragmatic-source.com>
2009-05-14 22:16:00 +0200

    Almost complete test suite
    * Remplace :all with :each in before/after functions
    * Add code coverage for App class with tons of mocks to prevent
      system calls from being done during tests.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -19,8 +19,17 @@
 
 require File.join(File.dirname(__FILE__),"spec_helper.rb")
 
+TINYDNS_LOG_SAMPLE = [
+  "01020304:681f:489a + 001c master.sample.com\n",
+  "10203040:fdfb:2e42 + 0001 www.foo.com\n",
+  "aabbccdd:8011:d739 + 0006 foo-consulting.fr\n",
+  "22446688:ed2a:8166 I 0006 foo.tv\n",
+  "11335577:8011:4d27 + 0006 pragmatic-foo.com\n",
+  "22446688:ed2a:7b79 I 0006 sample.com\n"
+]
+
 describe Multilog::App do
-  before(:all) do
+  before(:each) do
     sample_path = File.join(File.dirname(__FILE__), '..', 'samples')
     @test_root   = File.join(sample_path, 'root_spec_' + rand(2**16).to_s)
     FileUtils.rm_rf(@test_root)
@@ -32,7 +41,7 @@ describe Multilog::App do
     end
   end
 
-  after(:all) do
+  after(:each) do
     FileUtils.rm_rf(@test_root)
     FileUtils.rm_f(@config_file)
   end
@@ -53,20 +62,55 @@ describe Multilog::App do
     app.instance_eval { @axfr_root }.should == @test_root
   end
 
-  it "should popen 'multilog' on run" do
+  it "should popen 'multilog' and write to it" do
     app = Multilog::App.new([ '-c', @config_file, 't', './main' ])
     stdout_file = File.open(File.join(@test_root, 'stdout.log'), 'w+')
+
     IO.expects(:popen).with('multilog t ./main', 'w').returns(stdout_file)
-    STDIN.expects(:gets).twice.returns("@4000000049eff7f506811f7c d946b128:8011:4d27 + 0006 pragmatic-services.com\n", false)
+    STDIN.expects(:gets).returns(false)
+
     app.run
     stdout_file.rewind
     output = stdout_file.readlines
     output[0].should match(/^multilog-axfr v.* started.$/)
-    output[1].should match(/pragmatic-services/)
   end
 
-  it "should treat DNS NOTIFY messages" do
-    pending
+  it "should treat regular logs normally" do
+    app = Multilog::App.new([ '-c', @config_file, 't', './main' ])
+    stdout_file = File.open(File.join(@test_root, 'stdout.log'), 'w+')
+    
+    IO.expects(:popen).with('multilog t ./main', 'w').returns(stdout_file)
+    STDIN.expects(:gets).times(4).returns(*TINYDNS_LOG_SAMPLE[0..2]).then.returns(false)
+    Multilog::App.any_instance.expects(:system).never
+
+    app.run
+    stdout_file.rewind
+    output = stdout_file.readlines
+    output[0].should match(/^multilog-axfr v.* started.$/)
+    (1..3).each do |n|
+      output[n].should == TINYDNS_LOG_SAMPLE[n-1]
+    end
+  end
+
+  it "should handle 2 DNS NOTIFY messages" do
+    Multilog::SlaveZones.any_instance.expects(:authorized?).returns(true).twice
+
+    app = Multilog::App.new([ '-c', @config_file, 't', './main' ])
+    stdout_file = File.open(File.join(@test_root, 'stdout.log'), 'w+')
+    
+    IO.expects(:popen).with('multilog t ./main', 'w').returns(stdout_file)
+    STDIN.expects(:gets).times(4).returns(*TINYDNS_LOG_SAMPLE[3..5]).then.returns(false)
+    Multilog::App.any_instance.expects(:system).times(4)
+    Multilog::App.any_instance.expects(:fork).twice.returns(100, 200)
+    Process.expects(:detach).twice.with(any_of(100, 200))
+
+    app.run
+    stdout_file.rewind
+    output = stdout_file.readlines
+    output[0].should match(/^multilog-axfr v.* started.$/)
+    (1..3).each do |n|
+      output[n].should == TINYDNS_LOG_SAMPLE[n+2]
+    end
   end
 end
 
